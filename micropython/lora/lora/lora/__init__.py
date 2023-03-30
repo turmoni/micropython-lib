acc365c3616e304d863d071a9bb7ceefa1eee8db
Angus Gratton <angus@redyak.com.au>
2023-03-30 15:21:19 +1100

    lora: Improve the error message if no modem driver in installed.
    Also don't print the error if async is not installed, turns out
    it's not very easy to understand from context.

--- micropython/lora/lora/lora/__init__.py
+++ micropython/lora/lora/lora/__init__.py
@@ -10,16 +10,16 @@ ok = False  # Flag if at least one modem driver package is installed
 try:
     from .async_modem import AsyncModem  # noqa: F401
 except ImportError as e:
-    print(str(e))
-
+    # This is a bit ugly and couples to the ImportError string, but it gives better errors if there
+    # is something else wrong in the module (for example, a missing Python feature on this board.)
+    if "no module named 'lora." not in str(e):
+        raise
 
 try:
     from .sx126x import SX1261, SX1262  # noqa: F401
 
     ok = True
 except ImportError as e:
-    # This is a bit ugly and couples to the ImportError string, but it gives better errors if there
-    # is something else wrong in the module (for example, a missing Python feature on this board.)
     if "no module named 'lora." not in str(e):
         raise
 
@@ -42,6 +42,6 @@ if not ok:
     # The base 'lora' package has been installed but no modem driver. i.e. need
     # to install a package like lora-sx127x or lora-sx126x, as per
     # documentation.
-    raise ImportError("No LoRa modem driver installed")
+    raise ImportError("No LoRa modem driver installed. Install lora-sx127x or lora-sx126x.")
 
 del ok
